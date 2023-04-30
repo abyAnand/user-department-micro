97d1bad5bf36b856f7d0060c22047aedd29cc498
abyAnand <abianand382@gmail.com>
2023-04-30 19:57:12 +0530

    Added Cloud-Config-Server

--- cloud-gateway/src/main/java/io/service/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/io/service/cloudgateway/CloudGatewayApplication.java
@@ -1,12 +1,14 @@
 package io.service.cloudgateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
